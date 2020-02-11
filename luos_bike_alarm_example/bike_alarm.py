35b603145ac4819adeb12dd60095221000737b03
Yoan Mollard <ymollard@users.noreply.github.com>
2020-02-11 19:10:19 +0100

    Quick-and-dirty servoing of the orientation

--- luos_bike_alarm_example/bike_alarm.py
+++ luos_bike_alarm_example/bike_alarm.py
@@ -2,37 +2,46 @@
 
 import rclpy
 from rclpy.node import Node
+from sensor_msgs.msg import Imu
 from visualization_msgs.msg import Marker
 from rclpy.timer import Clock
 
 class BikeExampleNode(Node):
-    TOPIC_NAME = "/bike_alarm/markers"
+    TOPIC_NAME_MARKERS = "/bike_alarm/markers"
+    TOPIC_NAME_LUOS_IMU = "/gps/imu"
     def __init__(self):
         super().__init__('bike_alarm')
-        self.publisher_ = self.create_publisher(Marker, self.TOPIC_NAME, 10)
-        self.timer = self.create_timer(1, self._cb_publish_markers)
+        self.publisher_ = self.create_publisher(Marker, self.TOPIC_NAME_MARKERS, 10)
+        self.subscriber = self.create_subscription(Imu, self.TOPIC_NAME_LUOS_IMU, self._cb_imu_received, 10)
+        self.timer = self.create_timer(0.1, self._cb_publish_markers)
         self.clock = Clock()
 
-    def _cb_publish_markers(self):
-        bike = Marker()
-        bike.action = Marker.ADD
-        bike.text = "Luos bike"
-        bike.type = Marker.MESH_RESOURCE
-        #bike.header.stamp = self.clock.now()
-        bike.header.frame_id = "/world"
+        # Here is the bike to be published in RViz
+        self.bike = Marker()
+        self.bike.text = "Luos bike"
+        self.bike.type = Marker.MESH_RESOURCE
+        #self.bike.header.stamp = self.clock.now()
+        self.bike.header.frame_id = "/world"
         # FIXME: from ament_index_python import get_resources
         # TODO: How does this resource-thing now work?
-        bike.mesh_resource = "file:///home/yoan/ros2_ws/src/luos_bike_alarm_example/assets/bike.stl"
-        bike.scale.x = 1e-3
-        bike.scale.y = 1e-3
-        bike.scale.z = 1e-3
-        bike.color.r = 1.
-        bike.color.g = 1.
-        bike.color.b = 1.
-        bike.color.a = 1.
-        bike.pose.position.z = 3.
-        bike.pose.orientation.w = 1.
-        self.publisher_.publish(bike)
+        self.bike.mesh_resource = "file:///home/yoan/ros2_ws/src/luos_bike_alarm_example/assets/bike.stl"
+        self.bike.scale.x = 1e-3
+        self.bike.scale.y = 1e-3
+        self.bike.scale.z = 1e-3
+        self.bike.color.r = 1.
+        self.bike.color.g = 1.
+        self.bike.color.b = 1.
+        self.bike.color.a = 1.
+        self.bike.pose.position.z = 3.
+        self.bike.pose.orientation.w = 1.
+
+    def _cb_imu_received(self, imu):
+        # This callback is called when an Imu message is received on the specified topic 
+        self.bike.pose.orientation = imu.orientation
+
+    def _cb_publish_markers(self):
+        # This publisher is called at a regular rate
+        self.publisher_.publish(self.bike)
         self.get_logger().info("Publishing bike")
 
 def main():
