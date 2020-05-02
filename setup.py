ca9549c9008f2d6fdda5be4dfde7bc2c89bb8c2c
Yoan Mollard <ymollard@users.noreply.github.com>
2020-05-03 00:27:09 +0200

    Version bump

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ package_name = 'luos_bike_alarm_example'
 
 setup(
     name=package_name,
-    version='0.0.1',
+    version='0.1.0',
     packages=[package_name],
     data_files=[
         ('share/ament_index/resource_index/packages', ['resource/' + package_name]),
