d3cf87fffaf497b11dfcdfa83bf6f6d534a7a34d
Yoan Mollard <ymollard@users.noreply.github.com>
2020-05-02 16:24:44 +0200

    Add rgb led module and reset to default aliases

--- luos_bike_alarm_example/bike_alarm.py
+++ luos_bike_alarm_example/bike_alarm.py
@@ -1,6 +1,7 @@
 import rclpy
 from rclpy.node import Node
 from time import time
+from std_msgs.msg import ColorRGBA
 from sensor_msgs.msg import Imu
 from luos_msgs.msg import State
 from visualization_msgs.msg import Marker
@@ -8,6 +9,10 @@ from os.path import join
 from ament_index_python.packages import get_package_prefix
 from ament_index_python.resources import RESOURCE_INDEX_SUBFOLDER
 
+TOPIC_NAME_LUOS_IMU = "/Imu_mod/imu"
+TOPIC_NAME_LUOS_STATE = "/button_mod/events/changed"
+TOPIC_NAME_LUOS_COLOR = "/rgb_led_mod/variables/color/write"
+TOPIC_NAME_MARKERS = "/bike_alarm/markers"
 
 class BikeColor:
     # Represent the color of the bike marker in RViz according to its riding state
@@ -23,7 +28,7 @@ class BikeColor:
         self._update_time = time()
         self._on = True
         self._blink = self.BLINK_NONE
-        
+
     @property
     def color(self):
         now = time()
@@ -42,15 +47,11 @@ class BikeColor:
 
 class BikeExampleNode(Node):
     # The actual node of the bike riding application
-    TOPIC_NAME_MARKERS = "/bike_alarm/markers"
-    TOPIC_NAME_LUOS_IMU = "/Imu_mod/imu"
-    TOPIC_NAME_LUOS_STATE = "/lock/events/changed"
-    
     def __init__(self):
         super().__init__('bike_alarm')
-        self.publisher_ = self.create_publisher(Marker, self.TOPIC_NAME_MARKERS, 10)
-        self.subscriber_imu = self.create_subscription(Imu, self.TOPIC_NAME_LUOS_IMU, self._cb_imu_received, 10)
-        self.subscriber_button = self.create_subscription(State, self.TOPIC_NAME_LUOS_STATE, self._cb_state_received, 10)
+        self.publisher_ = self.create_publisher(Marker, TOPIC_NAME_MARKERS, 10)
+        self.subscriber_imu = self.create_subscription(Imu, TOPIC_NAME_LUOS_IMU, self._cb_imu_received, 10)
+        self.subscriber_button = self.create_subscription(State, TOPIC_NAME_LUOS_STATE, self._cb_state_received, 10)
         self.timer_bike = self.create_timer(0.1, self._cb_timer_bike)
         self.bike_color = BikeColor()
         self.state = "idle"   # The bike current state : ["idle", "ride", "alarm"]
@@ -68,8 +69,9 @@ class BikeExampleNode(Node):
         self.bike.pose.position.z = 3.
         self.bike.pose.orientation.w = 1.
 
+        self.publisher_color = self.create_publisher(ColorRGBA, TOPIC_NAME_LUOS_COLOR, 10)
         self.get_logger().info("This is the Luos bike sharing example. \
-Shake the IMU {} or press the State button {}".format(self.TOPIC_NAME_LUOS_IMU, self.TOPIC_NAME_LUOS_STATE))
+Shake the IMU {} or press the State button {}".format(TOPIC_NAME_LUOS_IMU, TOPIC_NAME_LUOS_STATE))
 
     def _cb_imu_received(self, msg):
         # This callback is called when an Imu message is received on the specified topic 
@@ -94,8 +96,15 @@ Shake the IMU {} or press the State button {}".format(self.TOPIC_NAME_LUOS_IMU, 
 
     def _cb_timer_bike(self):
         # This publisher is called at a regular rate
-        self.bike.color.r, self.bike.color.g, self.bike.color.b = self.bike_color.color
-        self.publisher_.publish(self.bike)
+        colour = self.bike_color.color
+        # Invert full black and full white for the Luos LED:
+        if colour == self.bike_color.WHITE:
+            luos_color = ColorRGBA() 
+        else:
+            luos_color = ColorRGBA(r=colour[0]*64, g=colour[1]*64, b=colour[2]*64)
+        self.publisher_color.publish(luos_color)   # Publish colour to the Luos color module
+        self.bike.color.r, self.bike.color.g, self.bike.color.b = colour
+        self.publisher_.publish(self.bike)         # Publish colour to the RViz marker
 
 def main():
     rclpy.init()
